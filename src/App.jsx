8ba41b304003308ebf787e87baec73a11a907e38
vaibhav2480 <vaibhavssc123@gmail.com>
2025-03-16 20:37:34 +0530

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './components/footersection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Footer/>
+    </div>
   )
 }
 
