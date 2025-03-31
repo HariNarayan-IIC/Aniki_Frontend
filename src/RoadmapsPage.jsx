97424f9126ace6499d6d696abd2b46b37538511b
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-03-31 19:42:32 +0530

    Change image

--- src/RoadmapsPage.jsx
+++ src/RoadmapsPage.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function RoadmapsPage() {
     return (
-        <section className="h-[calc(100vh-154px)] md:h-[calc(100vh-74px)] flex justify-center bg-gray-200">
+        <section className="h-[calc(100vh-154px)] md:h-[calc(100vh-74px)] flex justify-center ">
             <img 
                 src='/images/UnderConstruction.jpg' 
                 alt="404 Under Construction image"
+                className="m-2"
             >
             </img>
         </section>
