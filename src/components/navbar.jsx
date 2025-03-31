3d3571a58da56ab8d34bc7578cabbe2ec8b484c2
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-03-31 15:57:48 +0530

    change Login button color and recentered nav bar options

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <>
     <nav className="bg-white shadow-md py-4 px-3 md:px-6 flex justify-between items-center">
-      <div className="flex gap-4">
+      
       {/* Logo */}
       <div className="text-2xl font-bold italic">
         <NavLink to={"/"}>Aniki</NavLink>
@@ -22,12 +22,11 @@ const Navbar = () => {
         <li className="hover:text-gray-600 cursor-pointer"><NavLink to={"/communities"}>Communities</NavLink></li>
         <li className="hover:text-gray-600 cursor-pointer"><NavLink to={"/resources"}>Resources</NavLink></li>
       </ul>
-      </div>
-
+      
       {/* Buttons */}
       <div className="flex md:space-x-4 space-x-2">
         <Link to="signup"><button className="px-4 py-2 border rounded-lg hover:scale-105 transition-transform cursor-pointer">Signup</button></Link>
-        <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:scale-105 transition-transform cursor-pointer">Login</button>
+        <button className="px-4 py-2 bg-green-700 text-white rounded-lg hover:scale-105 transition-transform cursor-pointer">Login</button>
       </div>
 
       {/* Mobile Menu Toggle Button */}
