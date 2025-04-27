f0547e1b0def8a018f88391f3b7d33d297189ac6
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-04-27 19:12:25 +0530

    Removed proxy from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000'
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': `${import.meta.env.VITE_API_BASE_URL}`,
+  //   }
+  // },
   plugins: [react(), tailwindcss()],
 })
