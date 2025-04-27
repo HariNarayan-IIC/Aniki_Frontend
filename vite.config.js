be1f5c8b9f071c22e447e9af3b9584bf696af4a8
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-04-27 20:01:17 +0530

    Again

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': process.env.VITE_API_BASE_URL,
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': process.env.VITE_API_BASE_URL,
+  //   }
+  // },
   plugins: [react(), tailwindcss()],
 })
